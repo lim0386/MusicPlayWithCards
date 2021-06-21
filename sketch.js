7aa38e82ce1fb61b8f346c3de29b918242228139
trumpetYK <lim0386@gmail.com>
2021-06-21 15:18:44 +0900

    222

--- sketch.js
+++ sketch.js
@@ -96,6 +96,16 @@ function preload() {
   sf06 = loadSound('sf/SynthFunkMelody01.mp3');
   sf07 = loadSound('sf/SynthFunkMElody02.mp3');
   
+let button1;
+let button2;
+let button3;
+let button4;
+let button5;
+let button6;
+let button7;
+let button8;
+let button9;
+
   classifier = ml5.imageClassifier(imageModelURL + 'model.json');
 
   console.log("loaded");
@@ -111,6 +121,203 @@ function setup() {
   flippedVideo = ml5.flipImage(video)
   // Start classifying
   classifyVideo();
+  
+  button1 = new Clickable();
+  button1.locate(20, 20);
+  button1.width = 20;
+  button1.height = 20;
+  button1.text = "";
+  button1.stroke = "#ff0000";
+  button1.color = "#ff0000";
+  button1.onRelease = function () {
+    Eyelash01.play();
+
+    // Eyelash01.stop();
+  Eyelash02.stop();
+  Eyelash03.stop();
+  Eyelash04.stop();
+  Eyelash05.stop();
+  Eyelash06.stop();
+  Eyelash07.stop();
+
+  };
+
+  button2 = new Clickable();
+  button2.locate(50, 20);
+  button2.width = 20;
+  button2.height = 20;
+  button2.text = "";
+  button2.stroke = "#ff0000";
+  button2.color = "#ff0000";
+  button2.onPress = function () {
+    Eyelash02.play();
+    
+    Eyelash01.stop();
+  // Eyelash02.stop();
+  Eyelash03.stop();
+  Eyelash04.stop();
+  Eyelash05.stop();
+  Eyelash06.stop();
+  Eyelash07.stop();
+  };
+
+  button3 = new Clickable();
+  button3.locate(80, 20);
+  button3.width = 20;
+  button3.height = 20;
+  button3.text = "";
+  button3.stroke = "#ff0000";
+  button3.color = "#ff0000";
+  button3.onPress = function () {
+    Eyelash03.play();
+
+    Eyelash01.stop();
+  Eyelash02.stop();
+  // Eyelash03.stop();
+  Eyelash04.stop();
+  Eyelash05.stop();
+  Eyelash06.stop();
+  Eyelash07.stop();
+  };
+
+  button4 = new Clickable();
+  button4.locate(110, 20);
+  button4.width = 20;
+  button4.height = 20;
+  button4.text = "";
+  button4.stroke = "#ffa500";
+  button4.color = "#ffa500";
+  button4.onPress = function () {
+    Eyelash04.play();
+
+    Eyelash01.stop();
+  Eyelash02.stop();
+  Eyelash03.stop();
+  // Eyelash04.stop();
+  Eyelash05.stop();
+  Eyelash06.stop();
+  Eyelash07.stop();
+  };
+
+  button5 = new Clickable();
+  button5.locate(140, 20);
+  button5.width = 20;
+  button5.height = 20;
+  button5.text = "";
+  button5.stroke = "#ffa500";
+  button5.color = "#ffa500";
+  button5.onPress = function () {
+    Eyelash05.play();
+
+    Eyelash01.stop();
+  Eyelash02.stop();
+  Eyelash03.stop();
+  Eyelash04.stop();
+  // Eyelash05.stop();
+  Eyelash06.stop();
+  Eyelash07.stop();
+  };
+
+  button6 = new Clickable();
+  button6.locate(170, 20);
+  button6.width = 20;
+  button6.height = 20;
+  button6.text = "";
+  button6.stroke = "#7fff00";
+  button6.color = "#7fff00";
+  button6.onPress = function () {
+    Eyelash06.play();
+
+    Eyelash01.stop();
+  Eyelash02.stop();
+  Eyelash03.stop();
+  Eyelash04.stop();
+  Eyelash05.stop();
+  // Eyelash06.stop();
+  Eyelash07.stop();
+  };
+
+  button7 = new Clickable();
+  button7.locate(200, 20);
+  button7.width = 20;
+  button7.height = 20;
+  button7.text = "";
+  button7.stroke = "#7fff00";
+  button7.color = "#7fff00";
+  button7.onPress = function () {
+    Eyelash07.play();
+
+    Eyelash01.stop();
+  Eyelash02.stop();
+  Eyelash03.stop();
+  Eyelash04.stop();
+  Eyelash05.stop();
+  Eyelash06.stop();
+  // Eyelash07.stop();
+  }; 
+
+  button8 = new Clickable();
+  button8.locate(230, 20);
+  button8.width = 20;
+  button8.height = 20;
+  button8.text = "";
+  button8.stroke = "#000000";
+  button8.color = "#000000";
+  button8.onPress = function () {
+    Eyelash01.play();
+    Eyelash04.play();
+    Eyelash06.play();
+
+    // Eyelash01.stop();
+  Eyelash02.stop();
+  Eyelash03.stop();
+  // Eyelash04.stop();
+  Eyelash05.stop();
+  // Eyelash06.stop();
+  Eyelash07.stop();
+  }; 
+
+  button9 = new Clickable();
+  button9.locate(260, 20);
+  button9.width = 20;
+  button9.height = 20;
+  button9.text = "";
+  button9.stroke = "#000000";
+  button9.color = "#000000";
+  button9.onPress = function () {
+    Eyelash02.play();
+    Eyelash05.play();
+    Eyelash07.play();
+
+    Eyelash01.stop();
+  // Eyelash02.stop();
+  Eyelash03.stop();
+  Eyelash04.stop();
+  // Eyelash05.stop();
+  Eyelash06.stop();
+  // Eyelash07.stop();
+  }; 
+
+  button10 = new Clickable();
+  button10.locate(290, 20);
+  button10.width = 20;
+  button10.height = 20;
+  button10.text = "";
+  button10.stroke = "#000000";
+  button10.color = "#000000";
+  button10.onPress = function () {
+    Eyelash02.play();
+    Eyelash05.play();
+    Eyelash06.play();
+
+    Eyelash01.stop();
+  // Eyelash02.stop();
+  Eyelash03.stop();
+  Eyelash04.stop();
+  // Eyelash05.stop();
+  // Eyelash06.stop();
+  Eyelash07.stop();
+  }; 
 
   console.log("Play");
   // citypop01.play();
@@ -155,85 +362,100 @@ function setup() {
 }
 
 function draw() {
-  switch(song){
-    case 0:
-  background(0);
-  // Draw the video
-  image(flippedVideo, 0, 0);
+  button1.draw();
+  button2.draw();
+  button3.draw();
+  button4.draw();
+  button5.draw();
+  button6.draw();
+  button7.draw();
+  button8.draw();
+  button9.draw();
+  button10.draw();
+  // switch(song){
+  //   case 0:
+  // background(0);
+  // // Draw the video
+  // image(flippedVideo, 0, 0);
 
-  // Draw the label
-  fill(255);
-  textSize(16);
-  textAlign(CENTER);
-  // Draw the label
-  if(label === "Class 36" || label ===  ""){
-    text("No", width / 2, height - 4);
-  }else{
-    text("OK", width / 2, height - 4);
-  }
+  // // Draw the label
+  // fill(255);
+  // textSize(16);
+  // textAlign(CENTER);
+  // // Draw the label
+  // if(label === "Class 36" || label ===  ""){
+  //   text("No", width / 2, height - 4);
+  // }else{
+  //   text("OK", width / 2, height - 4);
+  // }
   
-  if(label === "1" || label === "2" || label === "3"){
-    beat = label;
-    if(mouseIsPressed){
-    song = 1;
-    }
-  }
-  // if(label === "8" || label === "9" || label === "10"){
+  // if(label === "1" || label === "2" || label === "3"){
   //   beat = label;
+  //   if(mouseIsPressed){
+  //   song = 1;
+  //   }
+  // }else{
+  //   song = 0;
+  // }
+  // // if(label === "8" || label === "9" || label === "10"){
+  // //   beat = label;
 
-  //   song = 2;
-  // }
-  // if(label === "15" || label === "16" || label === "17"){
-  //   beat = label;
-  //   song = 3;
-  // }
-  // if(label === "22" || label === "23" || label === "24"){
-  //   beat = label;
-  //   song = 4;
-  // }
-  // if(label === "29" || label === "30" || label === "31"){
-  //   beat = label;
-  //   song = 5;
-  // }
-  // text(label, width / 2, height - 4);
-  console.log(label);
+  // //   song = 2;
+  // // }
+  // // if(label === "15" || label === "16" || label === "17"){
+  // //   beat = label;
+  // //   song = 3;
+  // // }
+  // // if(label === "22" || label === "23" || label === "24"){
+  // //   beat = label;
+  // //   song = 4;
+  // // }
+  // // if(label === "29" || label === "30" || label === "31"){
+  // //   beat = label;
+  // //   song = 5;
+  // // }
+  // // text(label, width / 2, height - 4);
+  // console.log(label);
 
-  break;
+  // break;
       
-      case 1:
-        image(flippedVideo, 0, 0);
-      if(label === "4" || label === "5"){
-        if(mouseIsPressed){
-        melody = label;
-        // if(mouseIsPressed){
-          
-        }
-      }
-      if(label === "6" || label === "7"){
-        if(mouseIsPressed){
-
-        chord == label;
-        }
-      }
-      console.log(song, melody, chord);
-      break;
+  //     case 1:
+  //       image(flippedVideo, 0, 0);
+  //     if(label === "4" || label === "5"){
+  //       if(mouseIsPressed){
+  //       melody = label;
+  //       song = 2;
+  //       }
+  //     }else{
+  //       song = 1;
+  //     }
       
-      case 2:
+  //     break;
       
-      break;
+  //     case 2:
+  //       if(label === "6" || label === "7"){
+  //         if(mouseIsPressed){
+  //         chord == label;
+  //         song = 3;
+  //         }
+  //       }else{
+  //         song = 2;
+  //       }
+  //       console.log(song, melody, chord);
+  //     break;
       
-      case 3:
+  //     case 3:
       
-      break;
+  //     break;
       
-      case 4:
+  //     case 4:
       
-      break;
+  //     break;
       
-      case 5:
+  //     case 5:
       
-      break;
-  }
+  //     break;
+  // }
 }
 
 function classifyVideo() {
